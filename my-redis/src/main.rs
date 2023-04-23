d3e189e4709f3003541432695244dc2f12dbb8f9
isobung <1024682836@qq.com>
2023-04-23 13:43:50 +0800

    add ffmpeg play code && add md toc

--- my-redis/src/main.rs
+++ my-redis/src/main.rs
@@ -2,12 +2,13 @@ use mini_redis::{client, Result};
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    // open a connection to the mini-redis address
     let mut client = client::connect("127.0.0.1:6379").await?;
 
     client.set("hello", "world".into()).await?;
 
     let result = client.get("hello").await?;
 
-    println!("get result from server end: {:?}", result);
+    println!("got value from the server, result={:?}", result);
     Ok(())
 }
