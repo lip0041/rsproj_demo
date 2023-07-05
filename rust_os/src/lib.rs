2f249fa23e1c440c7f0356178d99f0e2eb935923
isobung <1024682836@qq.com>
2023-07-05 16:28:47 +0800

    add memory, stop at post-09，can't go on

--- rust_os/src/lib.rs
+++ rust_os/src/lib.rs
@@ -10,6 +10,7 @@ use core::panic::PanicInfo;
 pub mod gdt;
 pub mod interrupts;
 pub mod serial;
+pub mod memory;
 pub mod vga_buffer;
 
 pub fn init() {
@@ -80,4 +81,18 @@ pub extern "C" fn _start() -> ! {
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     test_panic_handler(info)
+}
+
+
+#[cfg(test)]
+use bootloader::{entry_point, BootInfo};
+
+#[cfg(test)]
+entry_point!(test_kernel_main);
+
+#[cfg(test)]
+fn test_kernel_main(_boot_info: &'static BootInfo) -> ! {
+    init();
+    test_main();
+    hlt_loop();
 }
--- rust_os/src/main.rs
+++ rust_os/src/main.rs
@@ -4,24 +4,73 @@
 #![test_runner(rust_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
-use rust_os::println;
+use rust_os::{println, memory::BootInfoFrameAllocator};
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    println!("Hello world{}", "!");
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use rust_os::memory;
+    use x86_64::{structures::paging::Translate, VirtAddr, structures::paging::Page};
+
+    println!("Hello, World{}", "!");
     rust_os::init();
 
-    use x86_64::registers::control::Cr3;
 
-    let (level_4_page_table, _) = Cr3::read();
-    println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+
+    // map an unused page
+    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+
+    // write the string `New!` to the screen through the new mapping
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
+
+    let addresses = [
+        0xb8000,
+        // code page
+        0x201008,
+        // stack page
+        0x0100_0020_1a10,
+        // virt mapped to phys address 0
+        boot_info.physical_memory_offset,
+    ];
+
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = mapper.translate_addr(virt);
+        println!("{:?} -> {:?}", virt, phys);
+    }
 
     #[cfg(test)]
     test_main();
+
     println!("It did not crash!");
     rust_os::hlt_loop();
 }
+
+// #[no_mangle]
+// pub extern "C" fn _start(boot_info: &'static BootInfo) -> ! {
+//     println!("Hello world{}", "!");
+//     rust_os::init();
+
+//     use x86_64::registers::control::Cr3;
+
+//     let (level_4_page_table, _) = Cr3::read();
+//     println!(
+//         "Level 4 page table at: {:?}",
+//         level_4_page_table.start_address()
+//     );
+
+//     #[cfg(test)]
+//     test_main();
+//     println!("It did not crash!");
+//     rust_os::hlt_loop();
+// }
 
 /// This function is called on panic.
 #[cfg(not(test))]
